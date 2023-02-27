8105e8f4cb22585783c12a4257c81b4f34ab9d29
Jian Wu <xcb479@alumni.ku.dk>
2023-02-28 00:16:31 +0800

    Convert tabs to spaces

--- Test.PosMonitor/TestableHub.cs
+++ Test.PosMonitor/TestableHub.cs
@@ -7,25 +7,25 @@ using Moq;
 
 namespace Test.PosMonitor
 {
-	public class TestableHub: PositionHub
-	{
-		public int Sent;
+    public class TestableHub: PositionHub
+    {
+        public int Sent;
 
-		public TestableHub(
-			Mock<ILogger<PositionHub>> mockLogger, Mock<PositionsMonitor> mockMonitor,
-			Mock<IHubCallerClients> mockClients, Mock<HubCallerContext> mockContext)
-			: base(mockLogger.Object, mockMonitor.Object)
-		{
-			Sent = 0;		
-			Clients = mockClients.Object;
-			Context = mockContext.Object;
-		}
+        public TestableHub(
+            Mock<ILogger<PositionHub>> mockLogger, Mock<PositionsMonitor> mockMonitor,
+            Mock<IHubCallerClients> mockClients, Mock<HubCallerContext> mockContext)
+            : base(mockLogger.Object, mockMonitor.Object)
+        {
+            Sent = 0;		
+            Clients = mockClients.Object;
+            Context = mockContext.Object;
+        }
 
         public override async Task Send(Position[] array, IClientProxy client)
         {
-			await Task.Run(() => {
-				Interlocked.Increment(ref Sent);
-			});
+            await Task.Run(() => {
+                Interlocked.Increment(ref Sent);
+            });
         }
     }
 }
